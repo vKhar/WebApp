e40e7bd0ae3427020afe141b255d05ab4496df0d
vKhar <khar.woh@gmail.com>
2018-11-22 17:27:45 +0800

    external json file

--- app.py
+++ app.py
@@ -1,33 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,json
+import os
 
 app = Flask("__name__")
-product_list = [
-    {
-        "name":"Xenova Rockie",
-        "description":"Roomy main compartment with our signature front pocket design",
-        "price":42.00,
-        "image":"images/backpack.png"
-    },
-        {
-        "name":"Zenon EOS6D",
-        "description":"Duis felis orci, pulvinar id metus ut, rutrum luctus orci",
-        "price":1392.00,
-        "image":"images/camera.png"
-    },
-        {
-        "name":"Schola V",
-        "description":"A classical design in matt finish. Durable, tough and resistent to heat and cold",
-        "price":342.00,
-        "image":"images/glasses.jpg"
-    },
-        {
-        "name":"Scott Digital Smart Band",
-        "description":"4/7 heart rate monitoring with Elevate™ wrist heart rate technology",
-        "price":365.00,
-        "image":"images/watch.png"
-    }
-]
-
+json_file_path=os.path.join(app.root_path,"static",'data.json')
+product_list = json.load(open(json_file_path))
 
 @app.route("/")
 def root():
