fde3436a7f1e8dfbb6e85a3c33e54fcad7b127d5
vKhar <khar.woh@gmail.com>
2018-11-28 14:32:34 +0800

    handle db constraints with flash and no image upload runtime error

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 ## Step 7: remove json file, json load and json save
 ## use sqlite to list products and add new product
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 import os
 import sqlite3
 
 app = Flask("__name__")
+app.secret_key="Secret"
 DB="inventory.db" ## created from sqlitebrowser
 
 @app.route("/")
@@ -19,12 +20,13 @@ def root():
 
 @app.route('/add',methods = ['POST', 'GET'])
 def add_product():
+    f=None
     if request.method == 'POST':
         try:
             name=request.form["name"]
             description=request.form["description"]
             price=request.form["price"]
-            f=request.files["image"]
+            f=request.files["image"] ## runtime error
             image="images/"+f.filename
 
             with sqlite3.connect(DB) as con:
@@ -34,17 +36,20 @@ def add_product():
                      (name,description,price,image))
                 con.commit()
             msg="Product added"
-            print(msg)
+            flash(msg)
             upload_file_path=os.path.join(app.root_path,"static","images",f.filename)
             f.save(upload_file_path)
-        except Exception as e:
-            con.rollback()
-            msg="Error in adding record"
-            print(msg, str(e))
-        finally:
             return redirect(url_for("root"))
+        except Exception as e:
+            msg="Error in adding record:"
+            if f == None:
+                msg="No image file selected"
+            else:
+                msg="Error in adding record:"
+            flash(msg+str(e))
+            return render_template("product_frm.html")           
     else:
-        return render_template("product_frm.html",result = None)
+        return render_template("product_frm.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
