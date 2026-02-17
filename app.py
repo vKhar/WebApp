7eb96226fcef583a37ef8a49d3c9c3c25d14ea74
vKhar <khar.woh@gmail.com>
2026-02-17 17:12:12 +0800

    Add insert product page with image file picker UI

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask("__name__")
 
-product_list=[
-    {"name":""}
+product_list = [
+    {"name": ""}
 ]
+
+
 @app.route("/")
 def root():
     return render_template("products.html")
+
+
+@app.route("/insert-product")
+def insert_product():
+    return render_template("insert_product.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
