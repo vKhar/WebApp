b887c6e3d133ac4dd875de9c1cd42255357beb16
vKhar <khar.woh@gmail.com>
2026-02-18 09:02:31 +0800

    remove codex code

--- app.py
+++ app.py
@@ -2,20 +2,10 @@ from flask import Flask, render_template
 
 app = Flask("__name__")
 
-product_list = [
-    {"name": ""}
-]
-
-
 @app.route("/")
 def root():
     return render_template("products.html")
 
 
-@app.route("/insert-product")
-def insert_product():
-    return render_template("insert_product.html")
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
